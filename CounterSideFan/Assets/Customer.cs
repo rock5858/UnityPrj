f13ad0558dcae85a60c059c2c54b907b92c8efc2
rock5858 <rock5858@snu.ac.kr>
2023-01-28 02:37:17 +0900

    CustomerManager 추가.

---
+++ CounterSideFan/Assets/Customer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:05ec68a22dbc34d61ae6410fa846c7fb327b6b7f45b402da8951825aab44518c
+size 945
---
+++ CounterSideFan/Assets/CustomerManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:52827b75179c5a813bda5b8c7a01fa421dfbc81d204e9211da97422c974da8b8
+size 1313
